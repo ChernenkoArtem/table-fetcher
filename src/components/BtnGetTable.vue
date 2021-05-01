c839a833c580fd9dfd17838c85aa2ec3d1b08d91
Artem Chernenko <fantom96fantom96@outlook.com>
2021-05-01 17:20:48 +0300

    2222

---
+++ src/components/BtnGetTable.vue
@@ -0,0 +1,26 @@
+<template>
+<button @click="btnDataHandler">Get data</button>
+</template>
+
+<script>
+export default {
+  name: "BtnGetData",
+  props:['getDataBtnClick'],
+  methods:{
+    btnDataHandler() {
+      this.getDataBtnClick()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button{
+  padding: 8px 25px;
+  border: 1px solid #000;
+  font: inherit;
+  color: inherit;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,69 @@
+<template>
+  <div v-if="formatedStocksData.length"
+      class="table">
+    <div class="table__titles">
+      <div class="cell">Stock</div>
+      <div class="cell">Current</div>
+      <div class="cell">Change</div>
+    </div>
+    <div
+        v-for="stock in formatedStocksData"
+        v-bind:key="stock.stocks"
+        class="table__main">
+      <div class="table__row">
+        <div class="cell">{{stock.stocks}}</div>
+        <div class="cell">{{stock.current}}</div>
+        <div
+            :class="{negativeChange: stock.change < 0}"
+            class="cell change">{{stock.change}}</div>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props:['formatedStocksData']
+}
+</script>
+
+<style lang="scss" scoped>
+.table{
+  width: 350px;
+  color: black;
+  margin: 0 auto;
+  margin-top: 20px;
+  border: 1px solid darkgray;
+  &__titles {
+    display: flex;
+    width: 100%;
+    font-weight: 600;
+    justify-content: space-between;
+  }
+  &__main{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .table__row{
+      display: flex;
+      width: 100%;
+      border-top: 1px solid darkgray;
+      .change{
+        color: green;
+      }
+      .negativeChange{
+        color: red;
+      }
+    }
+  }
+  .cell{
+    padding: 8px 15px;
+    width: 100%;
+  }
+
+
+
+}
+</style>
